104d486248fb7c1407f14ecc3b236f6c3ce14992
IrinaZagaynova <ayyyyrin@gmail.com>
2020-03-21 20:38:47 +0300

    remove_duplicates

---
+++ lab2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("Incorrect number of arguments!");
+                System.Console.WriteLine("Usage remove_duplicates.exe <input string>");
+                return 1;
+            }
+            string inputString = args[0];
+            inputString = new string(inputString.Distinct().ToArray());
+            System.Console.WriteLine(inputString);
+            return 0;
+        }
+    }
+}
