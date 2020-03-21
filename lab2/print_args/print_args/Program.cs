486a44d75781198100177d7896a61fad8fd1a7f2
IrinaZagaynova <ayyyyrin@gmail.com>
2020-03-21 20:37:46 +0300

    print_args

---
+++ lab2/print_args/print_args/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrintArgs
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                System.Console.WriteLine("Number of arguments: " + args.Length);
+                System.Console.Write("Arguments: " + String.Join(" ", args));
+            }
+            else
+            {
+                System.Console.WriteLine("No parameters were specified!");
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
