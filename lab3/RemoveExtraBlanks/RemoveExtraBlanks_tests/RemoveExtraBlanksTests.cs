4c4aebf55958a0cdbe9d851887782c60c0877072
IrinaZagaynova <ayyyyrin@gmail.com>
2020-03-28 21:56:49 +0300

    RemoveExtraBlanks

--- lab3/RemoveExtraBlanks/RemoveExtraBlanks_tests/RemoveExtraBlanksTests.cs
+++ lab3/RemoveExtraBlanks/RemoveExtraBlanks_tests/RemoveExtraBlanksTests.cs
@@ -1,21 +1,13 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace RemoveExtraBlanks_tests
+namespace RemoveExtraBlanksTests
 {
     [TestClass]
-    public class CheckParseArgs_tests
+    public class CheckParseArgsTests
     {
-        [TestMethod]
-        public void ParseArgs_NilArgs_Test()
-        {
-            string[] str = { };
-            string parameter = "";
-            bool result = RemoveExtraBlanks.Program.ParseArgs(str, ref parameter, ref parameter);
-            Assert.IsFalse(result);
-        }
 
         [TestMethod]
-        public void ParseArgs_ThreeArgs_Test()
+        public void Invalid_number_of_args()
         {
             string[] str = { "input.txt", "output.txt", "smth" }; ;
             string parameter = "";
@@ -24,7 +16,7 @@ namespace RemoveExtraBlanks_tests
         }
 
         [TestMethod]
-        public void ParseArgs_TwoArgs_Test()
+        public void Correct_number_of_args()
         {
             string[] str = { "input.txt", "output.txt"}; ;
             string parameter = "";
@@ -34,31 +26,28 @@ namespace RemoveExtraBlanks_tests
     }
 
     [TestClass]
-    public class CheckRemoveExtraSpaces_tests
+    public class CheckRemoveExtraSpacesTests
     {
         [TestMethod]
-        public void RemoveRepetitiveSpaces_StringWitExtraSpaces_Test()
+        public void Remove_duplicate_spaces_and_tabs()
         {
             string str = " string    with    extra spaces ";
             string expectedStr = " string with extra spaces ";
             string resultStr = RemoveExtraBlanks.Program.RemoveRepetitiveSpaces(str);
             Assert.AreEqual(expectedStr, resultStr);
         }
-
     }
 
     [TestClass]
-    public class RemoveExtraBlanksInLine_tests
+    public class RemoveExtraBlanksInLineTest
     {
         [TestMethod]
-        public void RemoveExtraBlanksInLine_StringWitExtraSpaces_Test()
+        public void Remove_spaces_and_tabs_from_the_beginning_and_end_of_line()
         {
             string str = "   string    with    extra spaces";
             string expectedStr = "string with extra spaces";
             RemoveExtraBlanks.Program.RemoveExtraBlanksInLine(ref str);
             Assert.AreEqual(expectedStr, str);
         }
-
     }
-
 }
